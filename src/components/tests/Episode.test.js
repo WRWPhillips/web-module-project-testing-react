5ffc1493b967eec933d449aee939ee86c715e48a
Will Phillips <42WPhillips42@gmail.com>
2022-01-24 18:42:15 -0600

    mvp completed

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -4,8 +4,20 @@ import { render, screen } from '@testing-library/react';
 import Episode from './../Episode';
 
 
-test("renders without error", () => {});
+test("renders without error", () => {
+  render(<Episode episode={{}} />);
+});
 
-test("renders the summary test passed as prop", ()=>{});
+test("renders the summary test passed as prop", () =>{
+  render(<Episode episode={{summary: 'summary test'}} />);
+  let summary = screen.queryByText(/summary test/i);
+  expect(summary).toBeInTheDocument();
+  expect(summary).toBeTruthy();
+  expect(summary).toHaveTextContent('summary test');
+});
 
-test("renders default image when image is not defined", ()=>{});
+test("renders default image when image is not defined", () =>{
+  render(<Episode episode={{summary: 'summary test', image: null}} />);
+  const image = screen.queryByAltText('https://i.ibb.co/2FsfXqM/stranger-things.png');
+  expect(image).toBeInTheDocument();
+});
